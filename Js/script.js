fcf4b74c1a5064dfda0cf6c0e8aff5e093d8da7a
Himanshu <himanshubomble91@gmail.com>
2023-06-16 00:31:38 +0530

    Initial commit

---
+++ Js/script.js
@@ -0,0 +1,18 @@
+function sendemail() {
+    Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "username",
+      Password: "password",
+      To: 'receiver@gmail.com',
+      From: document.getElementById("email").value,
+      Subject: "Contact Enquiry",
+      Body: "Name: " + document.getElementById("name").value
+        + "<br> Email: " + document.getElementById("email").value
+        + "<br> Phone no: " + document.getElementById("phone").value
+        + "<br> Mesage: " + document.getElementById("message").value
+
+
+    }).then(
+      message => alert("Succesfully Sent")
+    );
+  }
