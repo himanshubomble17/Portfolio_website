3c561db9ba6854d4c89f57d61c30a21b549e3ca6
Himanshu <himanshubomble91@gmail.com>
2023-07-27 02:31:38 +0530

    resume added

--- Js/script.js
+++ Js/script.js
@@ -1,25 +1,23 @@
 function sendemail() {
-    Email.send({
-      Host: "smtp.elasticemail.com",
-      Username: "username",
-      Password: "password",
-      To: 'receiver@gmail.com',
-      From: document.getElementById("email").value,
-      Subject: "Contact Enquiry",
-      Body: "Name: " + document.getElementById("name").value
-        + "<br> Email: " + document.getElementById("email").value
-        + "<br> Phone no: " + document.getElementById("phone").value
-        + "<br> Mesage: " + document.getElementById("message").value
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "username",
+    Password: "password",
+    To: 'receiver@gmail.com',
+    From: document.getElementById("email").value,
+    Subject: "Contact Enquiry",
+    Body: "Name: " + document.getElementById("name").value
+      + "<br> Email: " + document.getElementById("email").value
+      + "<br> Phone no: " + document.getElementById("phone").value
+      + "<br> Mesage: " + document.getElementById("message").value
 
 
-    }).then(
-      message => alert("Succesfully Sent")
-    );
-  }
-  
+  }).then(
+    message => alert("Succesfully Sent")
+  );
+}
 
 
 
 
-  
-  
+
